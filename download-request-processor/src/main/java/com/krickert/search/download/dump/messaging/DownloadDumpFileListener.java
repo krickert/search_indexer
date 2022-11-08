6807236ad098aa5b2e6ae021057007cb981fc377
kristianrickert <krickert@gmail.com>
2022-11-07 21:23:24 -0500

    created tests for the download service

--- download-request-processor/src/main/java/com/krickert/search/download/dump/messaging/DownloadDumpFileListener.java
+++ download-request-processor/src/main/java/com/krickert/search/download/dump/messaging/DownloadDumpFileListener.java
@@ -1,38 +1,51 @@
 package com.krickert.search.download.dump.messaging;
 
+import com.google.protobuf.Timestamp;
 import com.krickert.search.download.dump.component.FileDownloader;
 
+import com.krickert.search.model.constants.KafkaProtobufConstants;
+import com.krickert.search.model.util.ProtobufUtils;
 import com.krickert.search.model.wiki.DownloadFileRequest;
 import com.krickert.search.model.wiki.DownloadedFile;
 import io.micronaut.configuration.kafka.annotation.*;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Prototype;
 import io.micronaut.context.annotation.Value;
 import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.UUID;
+
+import static com.krickert.search.model.util.ProtobufUtils.now;
 
 @Slf4j
 @KafkaListener(threads = 3,
-        groupId = "download-request-listener")
+        groupId = "download-request-listener",
+        properties = @Property(name = KafkaProtobufConstants.SPECIFIC_CLASS_PROPERTY,
+                value = KafkaProtobufConstants.DOWNLOAD_FILE_REQUEST_CLASS))
 @Prototype
 public class DownloadDumpFileListener {
     final String downloadLocation;
     final FileDownloader fileDownloader;
     final DownloadedFileProcessingProducer producer;
+    final String hostname;
 
     @Inject
     public DownloadDumpFileListener(
             @Value("${wikipedia.download-location}")
             final String downloadLocation,
             final FileDownloader fileDownloader,
-            final DownloadedFileProcessingProducer producer) {
+            final DownloadedFileProcessingProducer producer)
+            throws UnknownHostException {
         this.downloadLocation = downloadLocation;
         this.fileDownloader = fileDownloader;
         this.producer = producer;
+        this.hostname = InetAddress.getLocalHost().getHostName();
     }
 
     @Topic("download-request")
@@ -53,11 +66,12 @@ public class DownloadDumpFileListener {
     private void downloadFileToDestination(DownloadFileRequest request) {
         try {
             String fullFilePath = this.downloadLocation + "/" + request.getFileName();
+            Timestamp downloadedStart = now();
             fileDownloader.download(
                     new URL(request.getUrl()),
                     new File(fullFilePath),
                     request.getErrorCheck().getErrorCheck());
-
+            Timestamp downloadedEnd = now();
             log.info("******SENDING TO PROCESS: " + request.getFileName());
             producer.sendFileProcessingRequest(UUID.randomUUID(),
                     DownloadedFile.newBuilder()
@@ -65,7 +79,9 @@ public class DownloadDumpFileListener {
                             .setFullFilePath(fullFilePath)
                             .setErrorCheck(request.getErrorCheck())
                             .setFileDumpDate(request.getFileDumpDate())
-                            .setServerName("localhost")
+                            .setServerName(hostname)
+                            .setDownloadStart(downloadedStart)
+                            .setDownloadEnd(downloadedEnd)
                             .build());
         } catch (MalformedURLException e) {
             log.error("message {} was sent ok but did not have a valid URL", request);
