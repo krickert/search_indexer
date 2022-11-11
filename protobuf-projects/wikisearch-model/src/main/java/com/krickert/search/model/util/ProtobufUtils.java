3e6ae6da9ad0b9d86705acbbb1fbf49d4b7c7d29
kristianrickert <krickert@gmail.com>
2022-11-11 14:11:11 -0500

    Added helper to save protocol buffers to disk - useful for creating some test data since it's hard to make

--- protobuf-projects/wikisearch-model/src/main/java/com/krickert/search/model/util/ProtobufUtils.java
+++ protobuf-projects/wikisearch-model/src/main/java/com/krickert/search/model/util/ProtobufUtils.java
@@ -1,8 +1,14 @@
 package com.krickert.search.model.util;
 
+import com.google.protobuf.Message;
 import com.google.protobuf.Timestamp;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.Instant;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ProtobufUtils {
     public static Timestamp now() {
@@ -13,5 +19,20 @@ public class ProtobufUtils {
     public static Timestamp stamp(long epochSeconds) {
         return Timestamp.newBuilder().setSeconds(epochSeconds)
                 .setNanos(0).build();
+    }
+
+    public static <T extends Message> void  saveProtobufToDisk(String dst,T item) throws IOException {
+        item.writeTo(new FileOutputStream(new File(dst)));
+    }
+
+    public static <T extends Message> void saveProtocoBufsToDisk(String dstPrefix, Collection<T> items) {
+        AtomicInteger i = new AtomicInteger();
+        items.forEach((item) -> {
+            try {
+                saveProtobufToDisk(dstPrefix + i.getAndIncrement() + ".bin", item);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
--- protobuf-projects/wikisearch-model/src/test/java/com/krickert/search/model/util/ProtobufUtilsTest.java
+++ protobuf-projects/wikisearch-model/src/test/java/com/krickert/search/model/util/ProtobufUtilsTest.java
@@ -1,7 +1,18 @@
 package com.krickert.search.model.util;
 
 import com.google.protobuf.Timestamp;
+import com.krickert.search.model.wiki.DownloadFileRequest;
+import com.krickert.search.model.wiki.ErrorCheck;
+import com.krickert.search.model.wiki.ErrorCheckType;
+import com.krickert.search.model.wiki.WikiSiteInfo;
+import org.apache.commons.io.FileUtils;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +32,60 @@ public class ProtobufUtilsTest {
         Timestamp stamp = ProtobufUtils.stamp(time);
         assertThat(stamp.getSeconds()).isEqualTo(time);
         assertThat(stamp.getNanos()).isEqualTo(0);
+    }
+
+    @Test
+    void testSaveProtoToDisk() throws IOException {
+        DownloadFileRequest request = DownloadFileRequest.newBuilder()
+                .setFileName("File Name").setUrl("someurl")
+                .setErrorCheck(ErrorCheck.newBuilder()
+                        .setErrorCheck("error check")
+                        .setErrorCheckType(ErrorCheckType.MD5).build())
+                .setFileDumpDate("20221111")
+                .setUrl("www.example.com").build();
+        ProtobufUtils.saveProtobufToDisk("somefile.bin", request);
+        File result = new File("somefile.bin");
+        assertThat(result)
+                .isNotNull()
+                .isFile();
+        DownloadFileRequest requestOnDisk = DownloadFileRequest.parseFrom(FileUtils.readFileToByteArray(result));
+        assertThat(request)
+                .isEqualTo(requestOnDisk);
+        FileUtils.forceDelete(result);
+    }
+
+    @Test
+    void testSaveProtosToDisk() throws IOException {
+        DownloadFileRequest request1 = DownloadFileRequest.newBuilder()
+                .setFileName("File Name")
+                .setErrorCheck(ErrorCheck.newBuilder()
+                        .setErrorCheck("error check")
+                        .setErrorCheckType(ErrorCheckType.MD5).build())
+                .setFileDumpDate("20221111")
+                .setUrl("www.example.com").build();
+        DownloadFileRequest request2 = DownloadFileRequest.newBuilder()
+                .setFileName("File Name2")
+                .setErrorCheck(ErrorCheck.newBuilder()
+                        .setErrorCheck("error check2")
+                        .setErrorCheckType(ErrorCheckType.SHA1).build())
+                .setFileDumpDate("20221112")
+                .setUrl("www.example2.com").build();
+
+        Collection<DownloadFileRequest> requests = Lists.newArrayList(request1, request2);
+        ProtobufUtils.saveProtocoBufsToDisk("request", requests);
+        File result0 = new File("request0.bin");
+        File result1 = new File("request1.bin");
+        assertThat(result0)
+                .isNotNull()
+                .isFile();
+        DownloadFileRequest request1OnDisk = DownloadFileRequest.parseFrom(FileUtils.readFileToByteArray(result0));
+        DownloadFileRequest request2OnDisk = DownloadFileRequest.parseFrom(FileUtils.readFileToByteArray(result1));
+        assertThat(request1)
+                .isEqualTo(request1OnDisk);
+        assertThat(request2)
+                .isEqualTo(request2OnDisk);
+        FileUtils.forceDelete(result0);
+        FileUtils.forceDelete(result1);
+
     }
 }
