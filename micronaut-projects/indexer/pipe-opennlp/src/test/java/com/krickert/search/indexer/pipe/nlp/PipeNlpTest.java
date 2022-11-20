fd76ab0af62d67075ebc2923831352c1a2729808
kristianrickert <krickert@gmail.com>
2022-11-20 09:05:45 -0500

    pipe-nlp: going to be a grpc service that does nlp processing via the opennlp library

---
+++ micronaut-projects/indexer/pipe-opennlp/src/test/java/com/krickert/search/indexer/pipe/nlp/PipeNlpTest.java
@@ -0,0 +1,21 @@
+package com.krickert.search.indexer.pipe.nlp;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class PipeNlpTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
