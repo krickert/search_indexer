c04bc9b43ac6a1331c471ab0697a07edf58150ea
kristianrickert <krickert@gmail.com>
2023-10-02 08:28:18 -0400

    Made the embeddings service and changed the model to support dynamic structures

--- services/wiki-downloader/src/main/java/com/krickert/search/wiki/article/processor/component/PipelineDocumentMapper.java
+++ services/wiki-downloader/src/main/java/com/krickert/search/wiki/article/processor/component/PipelineDocumentMapper.java
@@ -1,6 +1,9 @@
 package com.krickert.search.wiki.article.processor.component;
 
+import com.google.common.collect.Maps;
+import com.google.protobuf.Struct;
 import com.google.protobuf.Timestamp;
+import com.google.protobuf.Value;
 import com.krickert.search.model.pipe.PipeDocument;
 import com.krickert.search.model.wiki.WikiArticle;
 
@@ -8,6 +11,7 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
@@ -22,15 +26,18 @@ public class PipelineDocumentMapper {
                 .setTitle(wikiArticle.getTitle())
                 .setDocumentType(wikiArticle.getWikiType().name())
                 .setCreationDate(wikiArticle.getTimestamp());
-        pipeDocumentBuilder.putFields("namespace", wikiArticle.getNamespace())
-                .putFields("dump_date", wikiArticle.getDumpTimestamp());
+        Map<String, Value> wikiMetaData = Maps.newHashMap();
+        wikiMetaData.put("namespace", Value.newBuilder().setStringValue(wikiArticle.getNamespace()).build());
+        wikiMetaData.put("dump_date", Value.newBuilder().setStringValue(wikiArticle.getDumpTimestamp()).build());
+        wikiMetaData.put("date_parsed", Value.newBuilder().setStringValue(parseDateParsed(wikiArticle.getDateParsed())).build());
         if (isNotEmpty(wikiArticle.getText())) {
             pipeDocumentBuilder.setBody(wikiArticle.getText());
         }
         if (isNotEmpty(wikiArticle.getWikiText())) {
-            pipeDocumentBuilder.putFields("wiki_text", wikiArticle.getWikiText());
+            wikiMetaData.put("wiki_text", Value.newBuilder().setStringValue(wikiArticle.getWikiText()).build());
         }
-        pipeDocumentBuilder.putFields("date_parsed", parseDateParsed(wikiArticle.getDateParsed()));
+        Struct customFields = Struct.newBuilder().putAllFields(wikiMetaData).build();
+        pipeDocumentBuilder.putFields("wikiMetadata", customFields);
         return pipeDocumentBuilder.build();
     }
 
