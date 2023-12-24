846137e1c8470a32bfd587b10ec5a0f50c5fff8c
Kristian Rickert <krickert@gmail.com>
2023-12-24 09:48:22 -0500

    moved grpc-servies to be pipeline-services since we'll create other grpc services within the web-crawler project.  Also, starting the web-crawler project.

---
+++ services/web-crawler/crawler-job-creator/src/main/java/com/krickert/search/crawler/CrawlerJobCreatorCommand.java
@@ -0,0 +1,28 @@
+package com.krickert.search.crawler;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "crawler-job-creator", description = "...",
+        mixinStandardHelpOptions = true)
+public class CrawlerJobCreatorCommand implements Runnable {
+
+    @Option(names = {"-v", "--verbose"}, description = "...")
+    boolean verbose;
+
+    public static void main(String[] args) throws Exception {
+        PicocliRunner.run(CrawlerJobCreatorCommand.class, args);
+    }
+
+    public void run() {
+        // business logic here
+        if (verbose) {
+            System.out.println("Hi!");
+        }
+    }
+}
