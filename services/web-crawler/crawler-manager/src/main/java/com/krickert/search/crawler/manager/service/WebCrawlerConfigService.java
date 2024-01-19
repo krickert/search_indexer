69635bf50fd6117e1e5cb5d184dc652ca03c4479
Kristian Rickert <krickert@gmail.com>
2024-01-19 13:13:30 -0500

    Fixed broken test.  Added some more design the the protocol buffers.

--- services/web-crawler/crawler-manager/src/main/java/com/krickert/search/crawler/manager/service/WebCrawlerConfigService.java
+++
@@ -1,9 +0,0 @@
-package com.krickert.search.crawler.manager.service;
-
-import com.krickert.search.model.crawler.WebCrawlerGrpc;
-
-public class WebCrawlerConfigService extends WebCrawlerGrpc.WebCrawlerImplBase {
-
-
-
-}
