0dc6da6d9a1e4beb1900169b729d7fc10790dc15
Kristian Rickert <krickert@gmail.com>
2024-01-29 05:55:41 -0500

    Updated test so we can now have pipeline documents on disk with a boolean in the test.  Used to update the porotocol buffers testing dummy data.

--- services/wiki-downloader-services/wiki-article-to-pipedocument-processor/src/test/java/com/krickert/search/wiki/article/processor/component/PipelineDocumentMapperTest.java
+++ services/wiki-downloader-services/wiki-article-to-pipedocument-processor/src/test/java/com/krickert/search/wiki/article/processor/component/PipelineDocumentMapperTest.java
@@ -3,7 +3,11 @@ package com.krickert.search.wiki.article.processor.component;
 import com.google.common.collect.Lists;
 import com.google.protobuf.Timestamp;
 import com.krickert.search.model.pipe.PipeDocument;
+import com.krickert.search.model.util.ProtobufUtils;
 import com.krickert.search.model.wiki.WikiArticle;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +17,13 @@ import java.util.Collection;
 import static com.krickert.search.model.test.util.TestDataHelper.getFewHunderedArticles;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@MicronautTest
 class PipelineDocumentMapperTest {
     private static final Logger log = LoggerFactory.getLogger(PipelineDocumentMapperTest.class);
     PipelineDocumentMapper mock = new PipelineDocumentMapper();
+
+    @Value("${pipe.document.create_dummy_data}")
+    boolean createDummyData;
 
     @Test
     void testDateConversion() {
@@ -44,6 +51,10 @@ class PipelineDocumentMapperTest {
         Collection<String> pipeIds = Lists.newArrayList();
         mapperDocuments.forEach((document -> pipeIds.add(document.getId())));
         assertThat(articleIds).containsAll(pipeIds);
+
+        if (createDummyData) {
+            ProtobufUtils.saveProtocoBufsToDisk("pipe_document", mapperDocuments, 3);
+        }
     }
 
 
