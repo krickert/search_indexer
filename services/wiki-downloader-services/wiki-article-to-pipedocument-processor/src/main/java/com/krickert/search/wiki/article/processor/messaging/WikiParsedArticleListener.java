0a657fe776314a9194a551063ad8c93359e5bbd3
Kristian Rickert <krickert@gmail.com>
2024-01-29 23:14:20 -0500

    added feature to parse only article types so we can avoid all the crap we were parsing before it's processed.

--- services/wiki-downloader-services/wiki-article-to-pipedocument-processor/src/main/java/com/krickert/search/wiki/article/processor/messaging/WikiParsedArticleListener.java
+++ services/wiki-downloader-services/wiki-article-to-pipedocument-processor/src/main/java/com/krickert/search/wiki/article/processor/messaging/WikiParsedArticleListener.java
@@ -2,6 +2,7 @@ package com.krickert.search.wiki.article.processor.messaging;
 
 import com.krickert.search.model.constants.KafkaProtobufConstants;
 import com.krickert.search.model.wiki.WikiArticle;
+import com.krickert.search.model.wiki.WikiType;
 import com.krickert.search.wiki.article.processor.component.PipelineDocumentMapper;
 import io.micronaut.configuration.kafka.annotation.KafkaKey;
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
@@ -9,6 +10,7 @@ import io.micronaut.configuration.kafka.annotation.OffsetReset;
 import io.micronaut.configuration.kafka.annotation.Topic;
 import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Prototype;
+import io.micronaut.context.annotation.Value;
 import jakarta.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,10 +29,13 @@ public class WikiParsedArticleListener {
     private final PipeDocumentProducer pipeDocumentProducer;
     private final PipelineDocumentMapper pipeDocumentMapper;
 
+    private final Boolean parseOnlyArticles;
+
     @Inject
-    public WikiParsedArticleListener(PipeDocumentProducer pipeDocumentProducer) {
+    public WikiParsedArticleListener(PipeDocumentProducer pipeDocumentProducer, @Value("${wikipedia.parse-only-articles}") Boolean parseOnlyArticles) {
         this.pipeDocumentProducer = pipeDocumentProducer;
         this.pipeDocumentMapper = new PipelineDocumentMapper();
+        this.parseOnlyArticles = parseOnlyArticles;
     }
 
 
@@ -42,6 +47,10 @@ public class WikiParsedArticleListener {
                         String topic,
                         long timestamp) {
         log.debug("Got the request {} with key {}", request, key);
+        if (parseOnlyArticles && request.getWikiType() != WikiType.ARTICLE) {
+            log.info("Not sending {} with title {} because articles are only being processed.", request.getWikiType(), request.getTitle());
+            return;
+        }
         log.info("this {}:{} was sent with offset {} from partition {} from the {} topic at {}",
                 request.getId(), request.getTitle(), offset, partition, topic, timestamp);
 
