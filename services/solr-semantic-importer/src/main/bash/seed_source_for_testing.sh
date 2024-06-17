8ab7d43b6f69b93d82fc4d6ab5c01d3362eb3178
krickert <krickert@gmail.com>
2024-06-16 21:53:35 -0400

    added curl calls to crete the product source and destination

--- services/solr-semantic-importer/src/main/bash/seed_source_for_testing.sh
+++ services/solr-semantic-importer/src/main/bash/seed_source_for_testing.sh
@@ -2,6 +2,9 @@
 docker run --name my_solr7 -d -p 8983:8983 solr:7.7.3 bash -c "solr start -f -c"
 docker run --name my_solr9 -d -p 8984:8983 solr:9.6.1 bash -c "solr start -f -c"
 
+curl -L "http://localhost:8983/solr/admin/collections?action=CREATE&name=products_source&numShards=1&replicationFactor=1&maxShardsPerNode=1&collection.configName=_default"
+curl -L "http://localhost:8984/solr/admin/collections?action=CREATE&name=products_destination&numShards=1&replicationFactor=1&maxShardsPerNode=1&collection.configName=_default"
+
 BASE_URL="http://localhost:8983/solr/products_source/update"
 HEADER="Content-Type: application/json"
 
