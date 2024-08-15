4097d80c4bc9ddc910e3249629da77009443178c
krickert <krickert@gmail.com>
2024-08-14 20:52:28 -0400

    remove old directory

--- services/pipeline-services/enhancement/chunker/src/main/java/com/krickert/search/chunker/StringRequestRecord.java
+++
@@ -1,8 +0,0 @@
-package com.krickert.search.chunker;
-
-import io.micronaut.serde.annotation.Serdeable;
-
-
-@Serdeable
-public record StringRequestRecord(String text) {
-}
--- services/pipeline-services/enhancement/chunker/src/main/java/com/krickert/search/chunker/enhancers/Chunker.java
+++
@@ -1,7 +0,0 @@
-package com.krickert.search.chunker.enhancers;
-
-import java.util.List;
-
-public interface Chunker {
-    public List<String> chunk(String text);
-}
