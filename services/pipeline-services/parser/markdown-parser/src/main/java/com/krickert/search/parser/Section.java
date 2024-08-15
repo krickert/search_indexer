03e3446addb245ff58bd911adadcb214acc6d1fb
krickert <krickert@gmail.com>
2024-08-15 07:35:42 -0400

    Markdown parser is now externalized

--- services/pipeline-services/parser/markdown-parser/src/main/java/com/krickert/search/parser/Section.java
+++
@@ -1,7 +0,0 @@
-package com.krickert.search.parser;
-
-import io.micronaut.serde.annotation.Serdeable;
-
-@Serdeable
-public record Section (
-     String type, String body, int headingIndentation){}
