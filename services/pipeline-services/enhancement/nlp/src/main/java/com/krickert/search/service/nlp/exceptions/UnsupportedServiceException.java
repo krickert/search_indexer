4db78957afd79284e6c4feb06a9a7ecec721dbe8
krickert <krickert@gmail.com>
2024-08-14 22:21:45 -0400

    deleted old version of ner project

--- services/pipeline-services/enhancement/nlp/src/main/java/com/krickert/search/service/nlp/exceptions/UnsupportedServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.krickert.search.service.nlp.exceptions;
-
-public class UnsupportedServiceException extends RuntimeException {
-    public UnsupportedServiceException(String reason) {
-        super(reason);
-    }
-}
