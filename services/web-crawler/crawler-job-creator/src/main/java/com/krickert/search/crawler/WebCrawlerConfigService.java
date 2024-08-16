c87c0388188ae12f89b83d8432446419399a9802
krickert <krickert@gmail.com>
2024-08-16 05:56:06 -0400

    removed web job creator to externalize

--- services/web-crawler/crawler-job-creator/src/main/java/com/krickert/search/crawler/WebCrawlerConfigService.java
+++
@@ -1,8 +0,0 @@
-package com.krickert.search.crawler;
-
-import com.krickert.search.model.crawler.WebCrawlConfigurationGrpc;
-import jakarta.inject.Singleton;
-
-@Singleton
-public class WebCrawlerConfigService extends WebCrawlConfigurationGrpc.WebCrawlConfigurationImplBase {
-}
