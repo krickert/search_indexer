2949dedf744cec30aab6b06480fad80040e24043
krickert <krickert@gmail.com>
2024-08-15 23:44:08 -0400

    no more importer here

--- services/solr-semantic-importer/src/main/java/com/krickert/search/indexer/solr/HttpSolrSelectClient.java
+++
@@ -1,7 +0,0 @@
-package com.krickert.search.indexer.solr;
-
-import java.io.IOException;
-
-public interface HttpSolrSelectClient {
-    String getSolrDocs(Integer paginationSize, Integer pageNumber) throws IOException, InterruptedException;
-}
