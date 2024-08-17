4cdf2f16af50c2740433a657cab702359b4241fd
krickert <krickert@gmail.com>
2024-08-17 14:23:45 -0400

    removed old wiki services, replacing them with externalized ones.

--- services/wiki-downloader-services/wiki-download-dump-file-processor/src/main/java/com/krickert/search/download/dump/DownloadRequestProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.krickert.search.download.dump;
-
-import io.micronaut.runtime.Micronaut;
-
-public class DownloadRequestProcessor {
-    public static void main(String[] args) {
-        Micronaut.run(DownloadRequestProcessor.class, args);
-    }
-}
